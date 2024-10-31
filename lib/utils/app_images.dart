2edabcf4c606d3ac4cca84dfda25ec7e08d26d67
naimulhassan <developernaimul00@gmail.com>
2024-10-31 12:36:19 +0600

    init

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,12 @@
+class AppImages {
+  static const String baseurl = "assets/images" ;
+
+  static const String noData = "$baseurl/noData.png" ;
+  static const String noImage = "$baseurl/noImage.png" ;
+  static const String profile = "$baseurl/profile.png" ;
+
+
+
+
+
+}
